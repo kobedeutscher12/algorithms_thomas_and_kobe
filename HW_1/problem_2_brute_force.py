f9e6735af1bde85ae8cafd229091fb9232fc17b7
Thomas McKeown <tmckeown@sandiego.edu>
2025-02-21 12:48:58 -0800

    tehe

--- HW_1/problem_2_brute_force.py
+++ HW_1/problem_2_brute_force.py
@@ -11,13 +11,58 @@ A brute force approach which checks all possible subsets of intervals and checks
 A more efficient solution which reduces the time complexity significantly.
 
 
-For each of the solutions, you should provide a description of algorithm
-A discussion on how the second solution improved the time complexity.
+Description of Algorithm:
+This algorithm checks every possible combination of intervals for validity and then returns the largest number
+of intervals it could find. 
 '''
+class ProblemTwoBruteForce():
+
+    def __init__(self, intervalList):
+        #store the input list of tuples
+        self.intervals = intervalList
+
+    def overlapChecker(self, tuple1, tuple2):
+        """Check if the two given intervals overlap at all"""
+        if (tuple1[0] < tuple2[0] and tuple2[0] < tuple1[1]):
+            return False
+        
+        elif (tuple2[0] < tuple1[0] and tuple1[0] < tuple2[1]):
+            return False
+        
+        elif (tuple1[0] < tuple2[1] and tuple2[1] < tuple1[1]):
+            return False
+        
+        elif (tuple2[0] < tuple1[1] and tuple1[1] < tuple2[1]):
+            return False
+        
+        elif (tuple1[0] == tuple2[0] and tuple1[1] == tuple2[1]):
+            return False
+        
+        else:
+            return True
+        
+    def mostIntervalsBrute(self):
+        """Find largest set of tuples with brute force"""
+        maxSet = 0
+
+        #loop through every interval and check if it overlaps with any others
+        for i in self.intervals:
+            newSet = {i}
+            
+            for j in self.intervals:
+                
+                if self.overlapChecker(i, j):
+                    newSet.add(j)
+            
+            if (len(newSet) > maxSet):
+                maxSet = len(newSet)
+
+        return maxSet
 
 
 def main():
     ex1 = [(1, 3), (2, 5), (4, 6), (6, 8), (7, 9)]
+    print(ProblemTwoBruteForce(ex1).mostIntervalsBrute())
 
 if __name__ == "__main__":
     main()
